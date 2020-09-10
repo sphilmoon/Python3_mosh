c9dd32b4d49356044863c6ede6f9b2585e68ff79
sphilmoon <sphilmoon@gmail.com>
2020-09-10 19:33:46 +0900

    basic interactive input function

---
+++ 04_input.py
@@ -0,0 +1,11 @@
+# inputting your profile info:
+
+birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2020 - int(birth_year)
+print(type(age))
+print(age)
+
+body_weight_lbs = input('your current weight: ')
+body_weight_kg = 0.45 * int(body_weight_lbs)
+print(body_weight_kg)
