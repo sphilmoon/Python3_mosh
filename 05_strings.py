c23a477be4ac1472e59fc8bd9fea325c40592840
sphilmoon <sphilmoon@gmail.com>
2020-09-11 19:50:44 +0900

    defining variables called strings and understanding their index to print.

--- 05_strings.py
+++ 05_strings.py
@@ -3,5 +3,6 @@ another = course[:]
 print(another)
 
 name = 'Jennifer'
-print(name[1:-1])
-
+print(name[1:4]) # first parameter is inclusive, second is exclusive.
+print(name[:-2]) # printing 0th <= name < -2nd.
+print(name[0:-2]) # same as right above.
