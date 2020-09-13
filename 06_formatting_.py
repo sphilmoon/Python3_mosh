f4643342db0ff30cb5eb6656c4a78d8859f7e262
sphilmoon <sphilmoon@gmail.com>
2020-09-13 16:09:31 +0900

    string formatting.

---
+++ 06_formatting_.py
@@ -0,0 +1,13 @@
+first_name = 'Phil'
+last_name = 'Moon'
+message = first_name + ' [' + last_name + '] is a coder'
+msg = f'{first_name} [{last_name}] is a coder.'
+print(msg)
+
+
+
+
+subject = 'Python'
+platform = 'YouTube'
+message_2 = f"I'm learning {subject} on {platform}!"
+print(message_2)
---
+++ 07_formatting2.py
@@ -0,0 +1,17 @@
+course = 'Python for Beginners'
+print(len(course))
+print(course.upper())
+print(course)
+print(course.lower())
+
+print(course.find('n'))
+print(course.title('B'))
+print(course.replace('Beginners', 'Absolute Beginners'))
+print('python' in course)
+
+len()
+course.upper()
+course.lower()
+course.title()
+course.find()
+course.replace()
