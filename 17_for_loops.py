336b7d8202b822f31576ba9e96ce77dc279531a5
sphilmoon <sphilmoon@gmail.com>
2020-09-22 18:54:20 +0900

    for loops define another variable to enable the loop.

---
+++ 17_for_loops.py
@@ -0,0 +1,7 @@
+# collection = strings
+
+prices = [10, 20, 30]
+total = 0
+for dollars in prices: # defining dollars to add up the values in prices.
+    total += dollars # total = total + dollars (prices)
+print(f"Total: {total}") # formatted string.
