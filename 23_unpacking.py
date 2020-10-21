9b6a0446553a81714282739217937616da15094d
sphilmoon <sphilmoon@gmail.com>
2020-10-21 20:58:17 +0900

    added few comments.

--- 23_unpacking.py
+++ 23_unpacking.py
@@ -1,3 +1,4 @@
 coordinates = (2,4,3) # assign integers to my variable.
 x,y,z=coordinates # assign xyz values to my variable.
-print(y)
+print(y) # now I'm printing only the y value.
+print(x) # x value.
