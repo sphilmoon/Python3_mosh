eb33233ac1e681c5732b45c3d75ef9f311949784
sphilmoon <sphilmoon@gmail.com>
2020-10-27 20:02:48 +0900

    using a definition to pass the information.

---
+++ 27_parameters.py
@@ -0,0 +1,10 @@
+# passing information to the functions.
+
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name}, {last_name}!')
+    print('Welcome aboard')
+
+# now the actual output.
+print("Start")
+greet_user("Bruce", last_name= "Banner")
+print("Finish")
