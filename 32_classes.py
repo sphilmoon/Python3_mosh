0c44f234485b8660a07688246bf2f9c4196eda5d
sphilmoon <sphilmoon@gmail.com>
2020-11-10 18:40:01 +0900

    define within the main loop.

---
+++ 32_classes.py
@@ -0,0 +1,24 @@
+# number
+# Strings
+# Booleans
+# Lists
+# dictionaries
+
+class Point: 
+    def move(self):
+        print("move")
+        
+    def draw(self):
+        print("draw")
+        
+
+point1 = Point()
+point1.draw()
+point1.x =10
+point1.y = 20
+print(point1.x)
+point1.draw()
+
+point2 = Point()
+point2.x =2
+print(point2.x)
