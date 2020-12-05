3cdcb1adb6c6c33f1b61c48df608d6bac050cebc
sphilmoon <sphilmoon@gmail.com>
2020-12-05 09:19:18 +0900

    if/else statement for conversion.

--- 13_weight_converter.py
+++ 13_weight_converter.py
@@ -1,1 +1,11 @@
-Weight = input('Weight:  ')
+weight = int(input('Weight:  ')) # what about the floating input?
+print()
+unit = input("Indicate 'lbs' or 'kg': ") # any input error for sth else?
+print()
+
+if unit.lower() == 'lbs':
+        converted = weight * 0.45
+        print(f"You're {converted} kg.")
+else:
+    converted = weight / 0.45
+    print(f"You're {converted} lbs.")
